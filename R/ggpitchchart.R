bef9c62994b3c3f12059f826fd6bc14945d1450c
Sho SUZUKI <s.suzuki.2003@gmail.com>
2022-09-26 09:50:39 +0900

    ggpitchchart関数を作成．

---
+++ R/ggpitchchart.R
@@ -0,0 +1,100 @@
+#' @title **Pitch charts with ggplot2**
+#' @description 
+#' This function allows you to create pitch charts with ggplots given a data ball coordinates and The top and bottom of the strike zone.(The limits of the strike zone can be omitted)
+#' 
+#' @param data A data that includes ball coordinates.
+#' @param x_value The x coordinates. Typically plate_x. (If you put '-', it will be a catcher's point of view.)
+#' @param y_value The z coordinates. Typically plate_z. 
+#' @param sz_top The top of strike zone. Typically sz_top. If left blank, defaults to average height.
+#' @param sz_bot The top of strike zone. Typically sz_bot. If left blank, defaults to average height.
+#' @param color_value The categorical variable that you want the geom_points to base the color on. Pass as a string. If left blank, defaults to blue.
+#' @param density Chooses between a 2d density plot or a point plot. Defaults to FALSE.
+#' @param bin_size Size of bins used if use a density plot. Defaults to 5.
+#' @param scall_fill_palette You can use RColorBrewer's palette if use a density plot. Defaults to Blues.
+#' @param point_size Set the size of geom_point if used.
+#' @param frame Chooses from detailed, outline or none. Defaults to detailed.
+#' @return The pitch charts.
+#' 
+#' @importFrom ggplot2 ggplot geom_point geom_rect coord_fixed theme stat_density2d xlim ylim aes scale_fill_brewer
+#' @export
+
+ggpitchcharts <- function(data,
+                         x_value = "-plate_x",
+                         y_value = "plate_z",
+                         sz_top = NULL,
+                         sz_bot = NULL,
+                         color_value = NULL,
+                         density = FALSE,
+                         bin_size = 5,
+                         scall_fill_palette = "Blues",
+                         point_size = 1,
+                         frame = "detailed") {
+  
+  ball_zones<- data.frame(
+    x1 = rep(c(-2, 0), each = 2),
+    x2 = rep(c(0, 2), each = 2),
+    y1 = rep(c(-2, 0), 2),
+    y2 = rep(c(0, 2), 2)
+  )
+  
+  strike_zones <- data.frame(
+    x1 = rep(c(-0.83, -0.25, 0.25), each = 3),
+    x2 = rep(c(-0.25, 0.25, 0.83), each = 3),
+    y1 = rep(c(-0.83, -0.25, 0.25), 3),
+    y2 = rep(c(-0.25, 0.25, 0.83), 3)
+  )
+  
+
+  p0 <- ggplot()+
+      xlim(-2, 2) + xlab("") +
+      ylim(-2, 2) + ylab("") +
+      coord_fixed(ratio = 1) +
+      theme(panel.background = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(),
+            plot.title = element_text(face = "bold", size = 25), legend.position = "bottom", legend.text = element_text(size = 17), legend.title = element_text(size = 17))
+        
+  
+  if(!is.null(density)){
+    if(frame == "detailed"){
+      p1 <- p0+
+        geom_rect(data = ball_zones, aes(xmin = x1, xmax = x2, ymin = y1, ymax = y2), fill = "white", color = "gray")+
+        geom_rect(data = strike_zones, aes(xmin = x1, xmax = x2, ymin = y1, ymax = y2), fill = "white", color = "gray")
+    } 
+    if(frame == "outline"){
+      p1 <- p0
+    }
+    if(frame == "none"){
+      p1 <- p0
+    }
+    
+    if(!is.null(sz_top) && !is.null(sz_bot)){
+      p2 <- p1+
+        geom_point(data = data, aes(x = plate_x, y = (1.66*(plate_z-sz_bot)/(sz_top-sz_bot)-0.83), color = color_value))
+    }else{
+      p2 <- p1+
+        geom_point(data = data, aes(x = plate_x, y = (1.66*(plate_z-1.57)/(3.39-1.57)-0.83), color = color_value))
+    }
+  }else{
+    if(!is.null(sz_top) && !is.null(sz_bot)){
+      p1 <- p0+
+        geom_density_2d_filled(data = data,aes(x = plate_x, y = (1.66*(plate_z-sz_bot)/(sz_top-sz_bot)-0.83)),bins = bin_size, alpha = 0.5)+
+        scale_fill_brewer(palette = scall_fill_palette)
+    }else{
+      p1 <- p0+
+        geom_density_2d_filled(data = data,aes(x = plate_x, y = (1.66*(plate_z-1.57)/(3.39-1.57)-0.83)),bins = bin_size, alpha = 0.5)+
+        scale_fill_brewer(palette = scall_fill_palette)
+    }
+    
+    if(frame == "detailed"){
+      p2 <- p1+
+        geom_rect(data = ball_zones, aes(xmin = x1, xmax = x2, ymin = y1, ymax = y2), fill = "white", color = "gray")+
+        geom_rect(data = strike_zones, aes(xmin = x1, xmax = x2, ymin = y1, ymax = y2), fill = "white", color = "gray")
+    } 
+    if(frame == "outline"){
+      p2 <- p1
+    }
+    if(frame == "none"){
+      p2 <- p1
+    }
+  }
+  return(p2)
+}
